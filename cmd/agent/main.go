dee79127b9e83fb0c57b0eea2ebcacbecef9d037
malyavin.dd <malyavin.dd@outlook.com>
2023-07-26 17:33:52 +0300

    Added agent config implementation.

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import "time"
+
+type config struct {
+	metricReceiverAddress  string
+	sendTimeout            time.Duration
+	pollInterval           time.Duration
+	reportInterval         time.Duration
+	listOfCollectedMetrics []string
+}
+
+func (c *config) GetListOfCollectedMetrics() []string {
+	return c.listOfCollectedMetrics
+}
+
+func (c *config) GetMetricReceiverAddress() string {
+	return c.metricReceiverAddress
+}
+
+func (c *config) GetSendTimeout() time.Duration {
+	return c.sendTimeout
+}
