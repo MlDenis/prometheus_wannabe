92ee926d5d41806eea090808c32605efeeae74ec
DenisML <malyavin.dd@icloud.com>
2023-12-07 00:55:43 +0300

    Satisfied the linter's requirements.

--- internal/database/postgre/initializer.go
+++ internal/database/postgre/initializer.go
@@ -26,40 +26,40 @@ func initDB(ctx context.Context, connectionString string) (*sql.DB, error) {
 	return conn, nil
 }
 
-func getOrCreateMetricTypeId(ctx context.Context, conn *sql.DB, typeName string) (int, error) {
-	var typeId int
-	err := conn.QueryRowContext(ctx, "SELECT id FROM metricType WHERE name = $1", typeName).Scan(&typeId)
+func getOrCreateMetricTypeID(ctx context.Context, conn *sql.DB, typeName string) (int, error) {
+	var typeID int
+	err := conn.QueryRowContext(ctx, "SELECT id FROM metricType WHERE name = $1", typeName).Scan(&typeID)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			err = conn.QueryRowContext(ctx, "INSERT INTO metricType(name) VALUES ($1) RETURNING id", typeName).Scan(&typeId)
+			err = conn.QueryRowContext(ctx, "INSERT INTO metricType(name) VALUES ($1) RETURNING id", typeName).Scan(&typeID)
 		}
 		if err != nil {
 			return 0, err
 		}
 	}
-	return typeId, nil
+	return typeID, nil
 }
 
-func getOrCreateMetricId(ctx context.Context, conn *sql.DB, metricTypeName string, metricName string) (int, error) {
-	var metricId int
-	metricTypeId, err := getOrCreateMetricTypeId(ctx, conn, metricTypeName)
+func getOrCreateMetricID(ctx context.Context, conn *sql.DB, metricTypeName string, metricName string) (int, error) {
+	var metricID int
+	metricTypeID, err := getOrCreateMetricTypeID(ctx, conn, metricTypeName)
 	if err != nil {
 		return 0, err
 	}
-	err = conn.QueryRowContext(ctx, "SELECT id FROM metric WHERE name = $1 AND typeId = $2", metricName, metricTypeId).Scan(&metricId)
+	err = conn.QueryRowContext(ctx, "SELECT id FROM metric WHERE name = $1 AND typeId = $2", metricName, metricTypeID).Scan(&metricID)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			err = conn.QueryRowContext(ctx, "INSERT INTO metric(name, typeId) VALUES ($1, $2) RETURNING id", metricName, metricTypeId).Scan(&metricId)
+			err = conn.QueryRowContext(ctx, "INSERT INTO metric(name, typeId) VALUES ($1, $2) RETURNING id", metricName, metricTypeID).Scan(&metricID)
 		}
 		if err != nil {
 			return 0, err
 		}
 	}
-	return metricId, nil
+	return metricID, nil
 }
 
 func updateOrCreateMetric(ctx context.Context, conn *sql.DB, metricTypeName string, metricName string, metricValue float64) error {
-	metricId, err := getOrCreateMetricId(ctx, conn, metricTypeName, metricName)
+	metricId, err := getOrCreateMetricID(ctx, conn, metricTypeName, metricName)
 	if err != nil {
 		return err
 	}
